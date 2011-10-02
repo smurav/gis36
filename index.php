c9b90be48d9ebba13840a060ee3fe3a2a6d6f7de
Logina <LaKrista@mail.ru>
2011-10-03 01:06:50 +0400

    Test и начало работы

--- index.php
+++ index.php
@@ -1,36 +1,58 @@
-<!DOCTYPE html>
 <html>
 <head>
-  <meta charset='utf-8'>
-
   <title>smurav/gis36 @ GitHub</title>
+     <script language="JavaScript">
+   <!--
+     function over(imageName,imageSrc) {
+        if(document.images) {
+        document[imageName].src=imageSrc;
+       }
+     }
+     function preload(){
+      if (document.images) {
+       var imgsrc = preload.arguments;
+       arr=new Array(imgsrc.length);
+        for (var j=0; j<imgsrc.length; j++){
+        arr[j] = new Image;arr[j].src = imgsrc[j];
+       }
+      }
+     }
+   // -->
+  </script>
+
 
   <style type="text/css">
     body {
       margin-top: 1.0em;
-      background-color: #68ca74;
+      background-color: Burlywood3;
       font-family: Helvetica, Arial, FreeSans, san-serif;
-      color: #000000;
+      color: black;
     }
     #container {
       margin: 0 auto;
       width: 700px;
     }
-    h1 { font-size: 3.8em; color: #1B94E0; margin-bottom: 3px; }
+    h1 { font-size: 3.8em; color:black; margin-bottom: 3px; }
     h1 .small { font-size: 0.4em; }
     h1 a { text-decoration: none }
-    h2 { font-size: 1.5em; color: #1B94E0; }
-    h3 { text-align: center; color: #1B94E0; }
-    a { color: #1B94E0; }
+    h2 { font-size: 1.5em; color: black; }
+    h3 { text-align: center; color: black; }
+    a { color: black; }
     .description { font-size: 1.2em; margin-bottom: 30px; margin-top: 30px; font-style: italic;}
     .download { float: right; }
     pre { background: #000; color: #fff; padding: 15px;}
     hr { border: 0; width: 80%; border-bottom: 1px solid #aaa}
     .footer { text-align:center; padding-top:30px; font-style: italic; }
   </style>
+
 </head>
 
-<body>
+<BODY BGCOLOR=#F0F0FF onLoad="preload('air2.jpg','mer2.jpg','home2.jpg',
+'war2.jpg','dale2.jpg','analit2.jpg','otner2.jpg',
+'terra2.jpg','pix2_1.jpg','pix2_2.jpg','pix2_1.jpg')">
+
+
+
   <a href="http://github.com/smurav/gis36"><img style="position: absolute; top: 0; right: 0; border: 0;" src="http://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub" /></a>
 
   <div id="container">
@@ -42,15 +64,21 @@
         <img border="0" width="90" src="http://github.com/images/modules/download/tar.png"></a>
     </div>
 
-    <h1><a href="http://github.com/smurav/gis36">gis36</a>
-      <span class="small">by <a href="http://github.com/smurav">smurav</a></span></h1>
+    <h2><a href="http://github.com/smurav/gis36">Gis36</a>
+      <span class="small">by <a href="http://github.com/smurav">smurav</a></span></h2>
 
     <div class="description">
-      РЈС‡РµР±РЅС‹Р№ РїСЂРѕРµРєС‚ РїРѕ СЃРѕР·РґР°РЅРёСЋ Р“РРЎ
+      Учебный проект по созданию ГИС
     </div>
 
-    <h2>License</h2>
-<p>GPLv3</p>
+<div id="Layer1" style="width:800px; height:600px;
+z-index:1; left: 0px; top: 0px"> <img src="title2.jpg" width="800" height="600">
+</div>
+
+
+
+<h2>License:</h2>
+<p> GPLv3 </p>
 <h2>Authors</h2>
 <p>Sergey Muraviov (smurav@mail.ru)
 <br/>
@@ -77,6 +105,6 @@
 
   </div>
 
-  
+
 </body>
 </html>
